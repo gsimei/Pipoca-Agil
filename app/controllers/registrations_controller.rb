f508f540ac71d740356a4e4cfad9a939774cb681
RaquelFonsec <raque-leto@hotmail.com>
2023-05-18 01:43:25 +0200

    tela de signup

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :date_of_birth)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :date_of_birth)
+  end
+end
+def create
+  super do |resource|
+    if resource.errors.empty?
+      flash[:notice] = "Cadastro realizado com sucesso!"
+      # Redirecionar para a página desejada
+    else
+      flash[:alert] = "Ocorreu um erro durante o cadastro. Verifique os dados informados."
+      # Renderizar o formulário novamente com as mensagens de erro
+    end
+  end
+end
