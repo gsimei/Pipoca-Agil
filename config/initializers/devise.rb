cf742cf8de590117b213afa3ba0258e2c01a1f25
RaquelFonsec <raque-leto@hotmail.com>
2023-05-22 13:46:43 +0200

    facebook_login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -318,6 +318,6 @@ Devise.setup do |config|
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true
-    config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+    config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+
+  delete 'users/sign_out', to: 'devise/sessions#destroy'
 
   get "/admin", to: "pages#admin", as: "admin"
 end
