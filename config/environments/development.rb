13169a4c2ad04a7b1b154b6b11e79f8d4c3b9e37
RaquelFonsec <raque-leto@hotmail.com>
2023-05-31 18:13:16 +0200

    deploy_heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://pipocaagil.org/" }
+  config.action_mailer.default_url_options = { host: 'pipocaagil3.herokuapp.com', protocol: 'https' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
